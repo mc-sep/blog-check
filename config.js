236906c3126672941e1b0e1e61eba0e40a03f9cb
MC-Sep <mc-sep@outlook.com>
2024-10-12 14:43:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'MC-Sep 博客状态检测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2703449-729a7445dc25dc1d84b6e3ac',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://mc-sep.top'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/mc-sep'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.mc-sep.top'
+    },
+  ],
+};
